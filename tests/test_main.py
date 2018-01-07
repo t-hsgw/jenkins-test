e41d9ff17742f327783f4df844095cedccf2624b
thasegawa <thasegawa@airev.co.jp>
2018-01-07 14:04:35 +0900

    test update

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,13 @@
 import unittest
 import sys
 sys.path.append('../')
-from main import *
+from main import Main
 
 class TestMain(unittest.TestCase):
     def test_sum(self):
+        temp = Main()
         expected = 3
-        actual = Main.sum(1, 2)
+        actual = temp.sum(1, 2)
         self.assertEqual(expected, actual)
 
 if __name__== '__main__':
